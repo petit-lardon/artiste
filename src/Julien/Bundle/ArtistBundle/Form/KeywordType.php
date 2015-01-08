39d3cb08e786427d888262447bd0acbfc2757fa3
Julien <j.lavie@aramis-group.com>
2015-01-08 10:23:47 +0100

    CRUD pour l'entité Keyword

---
+++ src/Julien/Bundle/ArtistBundle/Form/KeywordType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Julien\Bundle\ArtistBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class KeywordType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('word')
+            ->add('works')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Julien\Bundle\ArtistBundle\Entity\Keyword'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'julien_bundle_artistbundle_keyword';
+    }
+}
