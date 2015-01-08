df26e20ff57328c08f95df2e95602f011c3fa727
Julien <j.lavie@aramis-group.com>
2015-01-08 10:24:13 +0100

    CRUD pour l'entité Step

---
+++ src/Julien/Bundle/ArtistBundle/Form/StepType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Julien\Bundle\ArtistBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class StepType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('achievedAt')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Julien\Bundle\ArtistBundle\Entity\Step'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'julien_bundle_artistbundle_step';
+    }
+}
